a699ffcc83e9d734bf3e7f514e1f5c5904d938af
rsidash <roman.sidash@kaspi.kz>
2026-03-09 19:14:48 +0500

    init commit

---
+++ src/main/java/kz/rsidash/mymarketapp/MyMarketAppApplication.java
@@ -0,0 +1,13 @@
+package kz.rsidash.mymarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMarketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/rsidash/mymarketapp/MyMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.rsidash.mymarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
